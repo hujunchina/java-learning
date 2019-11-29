4da5d5b4893d44e59ba133f68e1667d97f32334d
hujunchina <hujunchina@outlook>
2019-11-29 20:57:52 +0800

    structs

---
+++ hibernate/src/cn/edu/zju/service/UserService.java
@@ -0,0 +1,4 @@
+package cn.edu.zju.service;
+
+public class UserService {
+}
---
+++ spring/src/DAO/MainDao.java
@@ -0,0 +1,8 @@
+package DAO;
+
+// dao data access object
+public class MainDao {
+    public static void main(String[] args) {
+
+    }
+}
